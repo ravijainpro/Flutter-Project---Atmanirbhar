6a35c36bc7cef5926104502df07b4865ec7d0652
Ravi Jain <ravijainpro@gmail.com>
2020-07-09 11:19:23 +0530

    Updated

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/home/ravijain/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/ravijain/Documents/GitHub/Flutter-Project---Atmanirbhar"
+export "FLUTTER_APPLICATION_PATH=/home/ravijain/Downloads/Flutter-Project---Atmanirbhar-Subham"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
